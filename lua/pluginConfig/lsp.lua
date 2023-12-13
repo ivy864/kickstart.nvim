452730aef230fe1a34e81a3b0d95491e6e591215
vayers <violetlastname@gmail.com>
2023-12-12 22:41:16 -0500

    stop lsp from providing semantic tokens

--- lua/pluginConfig/lsp.lua
+++ lua/pluginConfig/lsp.lua
@@ -41,6 +41,8 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  _.server_capabilities.semanticTokensProvider = nil
 end
 
 -- document existing key chains
